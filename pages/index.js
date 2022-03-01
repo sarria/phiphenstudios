7330a807636748a9d6bf417cc4f929f2e5f5c69f
Juan F Sarria <juan@townsquaredigital.com>
2022-03-01 17:49:17 -0500

    Adding a message to the home page

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
 function Index() {
 	return (
-		<>
-		<h1>Home Page</h1>
-		<div>It deploys to vercel.com by just merging to Master. Coool!!!</div>
-		</>
+		<center>
+			<h1>Home Page</h1>
+			<div>It deploys to vercel.com by just merging to Master. Coool!!!</div>
+		</center>
 	)
 }
 
