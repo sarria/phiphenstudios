bc05f13fd3bae6b6e7a0dfbc93bed44d528236d8
Juan F Sarria <juan@townsquaredigital.com>
2022-03-01 17:44:06 -0500

    no message

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Index() {
-	return <div>Home Page</div>
+	return (
+		<>
+		<h1>Home Page</h1>
+		<div>It deploys to vercel.com by just merging to Master. Coool!!!</div>
+		</>
+	)
 }
 
 export default Index
