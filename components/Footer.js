eb3eb6baf8356fa32f821b067ab14cbde39af06e
Juan F Sarria <juan@townsquaredigital.com>
2022-03-05 12:10:31 -0500

    Fixing text align for mobile Placing temp footer image for desktop and mobile

--- components/Footer.js
+++ components/Footer.js
@@ -1,16 +1,24 @@
 import Image from 'next/image'
 import styles from './styles/footer.module.scss'
-import footerImg from '../public/footer.png'
+import footerImgDesktop from '../public/footer.png'
+import footerImgMobile from '../public/footer-mobile.png'
 
 const Footer = () => {
 	return (
 		<footer className={styles.root}>
 			<div className={styles.wrapper}>
-				<Image
-					alt=''
-					src={footerImg}
-					// layout="fill"
-				/>											
+				<div className={styles.desktop}>
+					<Image
+						alt=''
+						src={footerImgDesktop}
+					/>	
+				</div>
+				<div className={styles.mobile}>
+					<Image
+						alt=''
+						src={footerImgMobile}
+					/>
+				</div>
 			</div>
 		</footer>
 	)
